809a0ea8b348bfae60b4592a841d7c355230a48e
ugis <berzinsu@gmail.com>
2019-02-13 15:05:31 +0200

    Routing, compleate flow for checkout and orders page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -90,27 +90,15 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({loading:true})
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Ugis Berzins',
-                address: {
-                    street: 'Tallinas 83',
-                    zipCode: 'LV-1009',
-                    country: 'Latvia'
-                },
-                email: 'berzinsu@gmail.com'
-            },
-            deliveryMethod: 'fastest'
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i)+  '=' + encodeURIComponent(this.state.ingredients[i]))
         }
-        axios.post('/orders.json', order)
-        .then( resp => {
-            this.setState({loading:false,purchasing:false})
-        })
-        .catch( err => {
-            this.setState({loading:false,purchasing:false})
+        queryParams.push('price='+this.state.totalPrice)
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname:'/checkout',
+            search: queryString
         })
     }
 
